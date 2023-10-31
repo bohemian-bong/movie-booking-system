57f00af92b0acfaba17764fcaf420840943db9a2
aratrik1999 <46664969+aratrikchandra@users.noreply.github.com>
2023-10-31 19:40:01 +0530

    Creating Token and sending cookie as response

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,7 +16,9 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -32,10 +35,16 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({ user });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+  });
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
