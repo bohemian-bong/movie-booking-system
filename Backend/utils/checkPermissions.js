64798d091106c825744dd39076fc1116d3d4fe1a
aratrik1999 <46664969+aratrikchandra@users.noreply.github.com>
2023-11-16 20:21:45 +0530

    Update checkPermissions.js

--- Backend/utils/checkPermissions.js
+++ Backend/utils/checkPermissions.js
@@ -1,9 +1,6 @@
 const CustomError = require('../errors');
 
-const chechPermissions = (requestUser, resourceUserId) => {
-  // console.log(requestUser);
-  // console.log(resourceUserId);
-  // console.log(typeof resourceUserId);
+const checkPermissions = (requestUser, resourceUserId) => {
   if (requestUser.role === 'admin') return;
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
@@ -11,4 +8,4 @@ const chechPermissions = (requestUser, resourceUserId) => {
   );
 };
 
-module.exports = chechPermissions;
+module.exports = checkPermissions;
