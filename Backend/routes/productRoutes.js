0dc4e18f8f9ea5c9120c1e04e94191acd972587c
aratrik1999 <46664969+aratrikchandra@users.noreply.github.com>
2023-11-16 20:08:28 +0530

    routes for accessing movies

---
+++ Backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require('../controllers/productController');
+
+const { getSingleProductReviews } = require('../controllers/reviewController');
+const { getSingleProductShowTimes } = require('../controllers/showTimeController');
+
+router
+  .route('/')
+  .post([authenticateUser, authorizePermissions('admin')], createProduct)
+  .get(getAllProducts);
+
+router
+  .route('/uploadImage')
+  .post([authenticateUser, authorizePermissions('admin')], uploadImage);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+
+router.route('/:id/reviews').get(getSingleProductReviews);
+router.route('/:id/showtimes').get(getSingleProductShowTimes);
+
+module.exports = router;
